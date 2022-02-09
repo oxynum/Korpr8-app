0c96784c460b752642fa28581769b260cd28c6ca
A-Laure_DeV <77326841+AnneLaureSIEVERING@users.noreply.github.com>
2022-02-09 13:29:18 +0100

    init app

---
+++ android/app/src/main/java/com/korpr8/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.korpr8.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.korpr8.app',
+  appName: 'Korpr8',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
